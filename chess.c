0c715a59bc861bb7735354bc1f2814589bed6c3d
Litovkin Alexander <slitovkin0512@yandex.ru>
2022-02-14 10:04:49 +0700

    Code style chess.c

--- chess.c
+++ chess.c
@@ -2,7 +2,8 @@
 
 void printField(char chessField[8][8]);
 
-int main() {
+int main() 
+{
     // initializing the chessboard
     char chessField[8][8] = {{'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
 			     {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
@@ -17,11 +18,14 @@ int main() {
     return 0;
 }
 
-void printField(char chessField[8][8]) {
+void printField(char chessField[8][8]) 
+{
     // printing the chessboard
-    for (int i = 0; i < 8; i++) {
+    for (int i = 0; i < 8; i++) 
+    {
         printf("%d ", 8 - i);
-	for (int j = 0; j < 8; j++) {
+	for (int j = 0; j < 8; j++) 
+	{
 	    printf("%c ", chessField[i][j]);
 	}
 	printf("\n");
